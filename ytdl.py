103c93af3aa9b65a96c8a154062d2513e2738c37
Katakuari <katakuarii@gmail.com>
2022-05-12 21:56:26 +0200

    Removed functions and threading

--- ytdl.py
+++ ytdl.py
@@ -1,9 +1,9 @@
-﻿import webbrowser
-import PySimpleGUI as sg
-import threading
-import os
-import yt_dlp
+﻿import webbrowser, os, time
 from pathlib import Path
+
+import PySimpleGUI as sg    # python3 -m pip install -U PySimpleGUI
+import yt_dlp               # python3 -m pip install -U yt_dlp
+
 
 ######################################## GLOBAL VARS ########################################
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -11,49 +11,13 @@ ffmpeg = f'{ROOT_DIR}\\ffmpeg\\bin'
 
 yt_dlp.utils.bug_reports_message = lambda: ''
 
-######################################## FUNCTIONS ########################################
-class Logger:
-    def debug(self, msg):
-        # For compatibility with youtube-dl, both debug and info are passed into debug
-        # You can distinguish them by the prefix '[debug] '
-        if msg.startswith('[debug] '):
-            pass
-        else:
-            self.info(msg)
-
-    def info(self, msg):
-        pass
-
-    def warning(self, msg):
-        pass
-
-    def error(self, msg):
-        print(msg)
-
-
-def ytdl(ytlink, config):
-    with yt_dlp.YoutubeDL(config) as ydl:
-        threading.Condition().acquire()
-        ydl.download([f'{ytlink}'])
-        threading.Condition().release()
-        threading.Condition().notify_all()
-        
-
-def ytdlupdate():
-    # TODO: Make this work
-    try:
-        yt_dlp.run_update()
-    except ValueError as e:
-        print(f'Update failed with reason:\n{e}')
-
 
 def main():
 ######################################## PySimpleGUI ########################################
     sg.theme('Dark Blue 3')
 
     menubar_layout = [
-        ['&File', ['&Update YTDL', '&Quit']],
-        ['&Help', ['&Github', '&About']]
+        ['&Quit', '&Github', '&About']
     ]
 
     leftCol_layout = [
@@ -61,26 +25,28 @@ def main():
             [sg.Radio('MP4 (Video)', 1, key='FORMAT_MP4', enable_events=True, default=True)],
             [sg.Radio('M4A (Audio)', 1, key='FORMAT_M4A', enable_events=True)],
             [sg.Radio('MP3 (Audio)', 1, key='FORMAT_MP3', enable_events=True)],
-            [sg.Checkbox('Keep Video after processing', key='CB_KEEP', enable_events=True, default=False)]
+            [sg.Checkbox('Keep files after processing', key='CB_KEEP', enable_events=True, default=False)]
         ])],
         [sg.Frame('Download destination', [
             [sg.Radio('Downloads', 2, key='DEST_DOWNLOADS', enable_events=True, default=True)],
             [sg.Radio('Custom destination', 2, key='DEST_CUSTOM', enable_events=True),
-             sg.FolderBrowse(button_text='Browse...', key='DEST_CUSTOM_SEL', target='DEST_CUSTOM_CUR', initial_folder="C:/", enable_events=True)],
+            sg.FolderBrowse(button_text='Browse...', key='DEST_CUSTOM_SEL', target='DEST_CUSTOM_CUR', initial_folder="C:/", enable_events=True)],
             [sg.Text(size=(40, 1), key='DEST_CUSTOM_CUR', relief=sg.RELIEF_SUNKEN)]
         ])]
     ]
 
     rightCol_layout = [
         [sg.Text('Youtube link:'), sg.Input(key='YT_LINK')],
-        [sg.Button('Download', key='B_DOWNLOAD', expand_x=True, enable_events=True)],
-        [sg.Multiline(key='CON_OUT', size=(60, 12), autoscroll=True, do_not_clear=True, auto_refresh=True)]
+        [sg.Button('Download', key='B_DOWNLOAD', expand_x=True, enable_events=True, disabled=False)],
+        [sg.Frame('Status', [
+            [sg.Text(text='Click "Download" to start downloading', key='STATUS', enable_events=True, auto_size_text=True, relief=sg.RELIEF_FLAT, justification='center', expand_x=True, expand_y=True)]
+        ], expand_x=True, expand_y=True)]
     ]
 
     layout = [
         [sg.Menubar(menubar_layout)],
         [sg.Column(leftCol_layout, expand_y=True, expand_x=False, element_justification='center'),
-         sg.Column(rightCol_layout, expand_y=True, expand_x=False, element_justification='left')]
+        sg.Column(rightCol_layout, expand_y=True, expand_x=False, element_justification='center')]
     ]
 
     window = sg.Window('Youtube-dl PySimpleGUI', layout, border_depth=1, resizable=False, finalize=True)
@@ -97,14 +63,9 @@ def main():
             break
 
         # Menubar
-        if event == 'Update YTDL': 
-            updthread = threading.Thread(target=ytdlupdate)
-            sg.Popup('YT-DLP now updating...')
-            updthread.start()
-            updthread.join()
-            
-        if event == 'About': sg.PopupOK('A Python script written by Katakuari.\nTwitter: @ItsKatakuari\nGithub: https://github.com/Katakuari', title='About ytdl.py')
+        if event == 'About': sg.PopupOK('A Python script written by Katakuari.\nGithub: https://github.com/Katakuari', title='About ytdl.py')
         if event == 'Github': webbrowser.open(url="https://github.com/Katakuari/ytdl-win-scripts")
+
 
         # Left column: Format button functions
         if values['FORMAT_MP4'] is True:
@@ -114,7 +75,8 @@ def main():
                 'source_address': '0.0.0.0',
                 'ffmpeg_location': f'{ffmpeg}',
                 'newline': True,
-                'logger': Logger(),
+                'keepvideo': False,
+                #'logger': Logger,
             }
 
         if values['FORMAT_M4A'] is True:
@@ -124,11 +86,8 @@ def main():
                 'source_address': '0.0.0.0',
                 'ffmpeg_location': f'{ffmpeg}',
                 'newline': True,
-                'logger': Logger(),
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'm4a',
-                    'preferredquality': '192', }]
+                'keepvideo': False,
+                #'logger': Logger,
             }
 
         if values['FORMAT_MP3'] is True:
@@ -138,7 +97,8 @@ def main():
                 'source_address': '0.0.0.0',
                 'ffmpeg_location': f'{ffmpeg}',
                 'newline': True,
-                'logger': Logger(),
+                'keepvideo': False,
+                #'logger': LOGGER,
                 'postprocessors': [{
                     'key': 'FFmpegExtractAudio',
                     'preferredcodec': 'mp3',
@@ -146,7 +106,8 @@ def main():
             }
 
         if values['CB_KEEP'] is True: ydl_opts['keepvideo'] = True
-        if values['CB_KEEP'] is False: ydl_opts.pop("'keepvideo'", 'keep not found')
+        if values['CB_KEEP'] is False: ydl_opts['keepvideo'] = False
+
 
         # Left column: Destination button functions
         # Set Radio to custom destination upon choosing folder
@@ -162,18 +123,24 @@ def main():
             dldest = str(Path.home())+"\Downloads"
             os.chdir(dldest)
 
+
         # Download action
         if event == 'B_DOWNLOAD':
-            if (values['YT_LINK'] == ''): sg.PopupOK('Please insert YouTube link!', title='ERROR'); continue
+            if (values['YT_LINK'] == ''): sg.Popup('Please insert link starting with "https://"!', no_titlebar=True, background_color='darkred'); continue
+            if not (values['YT_LINK'].startswith('https://')): sg.Popup('Please insert link starting with "https://"!', no_titlebar=True, background_color='darkred'); continue
 
-            dlthread = threading.Thread(target=ytdl, args=(values['YT_LINK'], ydl_opts), daemon=True)
-            dlthread.start()
-            #dlthread.join()
+            window['STATUS'].update('Downloading...')
+            window['B_DOWNLOAD'].update(disabled=True)
+            time.sleep(1)
 
-            window['CON_OUT'].update(Logger())
+            with yt_dlp.YoutubeDL(ydl_opts, 'no_verbose_header') as ydl:
+                ydl.download(f"{values['YT_LINK']}")
+            
+            window['B_DOWNLOAD'].update(disabled=False)
+            window['STATUS'].update('Download finished!')
 
 
-        print(ydl_opts)
+        #print(ydl_opts)
         #print(event, values)
 
     window.close()
